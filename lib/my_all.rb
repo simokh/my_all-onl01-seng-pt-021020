db4b6067303b5eb3a9611c79af4fa0fde3d79210
Simo Khouili <m.simokhouili@gmail.com>
2020-02-22 01:10:21 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -8,10 +8,9 @@ def my_all?(collection)
     i = 1 + i
   end
 
- if block_return_values.include?
-   (false)
-    false
-  else
-    true
-  end
-end
+  if block_return_values.include?(false)
+     false
+   else
+     true
+   end
+ end
